bef6aa27697e9e769a842ae191cc559c76fe78bc
Bruno Talanski <brn.talanski@gmail.com>
2020-11-21 16:38:16 -0400

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -3,3 +3,63 @@
 #
 # To do
 #
+
+cd ..
+
+cd project/plugins/accounts-admin
+composer install
+cd ../../..
+
+cd project/plugins/accl
+composer install
+cd ../../..
+
+cd project/plugins/admin
+composer install
+npm install
+gulp
+cd ../../..
+
+cd project/plugins/form
+composer install
+npm install
+gulp
+cd ../../..
+
+cd project/plugins/form-admin
+composer install
+cd ../../..
+
+cd project/plugins/icon
+composer install
+npm install
+gulp
+cd ../../..
+
+cd project/plugins/jquery
+composer install
+npm install
+gulp
+cd ../../..
+
+cd project/plugins/phpmailer
+composer install
+cd ../../..
+
+cd project/plugins/site
+composer install
+cd ../../..
+
+cd project/plugins/themes-admin
+composer install
+cd ../../..
+
+cd project/plugins/twig
+composer install
+cd ../../..
+
+cd project/themes/noir
+npm install
+gulp
+cd ../../..
+
